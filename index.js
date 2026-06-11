f3cc2033a9f63184d693966b870cd84c79fee713
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-06-11 15:07:34 +0600

    featured api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-  
+
 const dotenv = require("dotenv");
 dotenv.config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 const uri = process.env.MONGODB_URI;
 
-app.use(cors());  
-app.use(express.json());      
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,7 +21,36 @@ const client = new MongoClient(uri, {
   },
 });
 
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("studynook").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
 
+    const db = client.db("studynook");
+
+    const roomCollection = db.collection("rooms");
+
+    
+
+    app.get("/api/rooms/featured", async (req, res) => {
+      const rooms = await roomCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(rooms);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("This is studyNook server");
